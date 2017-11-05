498dd1b3f2393b7f48565ebb8ce54a96c06188d0
Saucy <XFizzer@users.noreply.github.com>
2017-11-04 20:20:57 -0800

    Update

--- src/PrimeItems/Main.php
+++ src/PrimeItems/Main.php
@@ -7,6 +7,6 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase{
   
     public function onEnable(){
-            $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
     }
 }
