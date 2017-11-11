853ccbe1a8b8e47fb29281aa7a2450ff98ffd5ed
Enrick3344 <enrico3815@hotmail.fr>
2017-11-11 13:50:13 -0500

    Create ServerCommand.php

---
+++ src/PrimeItems/ServerCommand.php
@@ -0,0 +1,36 @@
+<?php 
+
+namespace PrimeItems;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+
+class ServerCommand extends Command{
+
+    /** @var Main */
+    private $plugin;
+    /**
+     * @param Main $plugin
+    */
+    
+    public function __construct(Main $plugin){
+        parent::__construct("servers", "Display the Servers Menu", null, ["server"]);
+        $this->setPermission("primeitems.server");
+        $this->plugin = $plugin;
+    }
+    
+    public function execute(CommandSender $sender, string $label, array $args) : bool{
+        $player = $sender;
+        if(!($sender instanceof Player)){
+            $sender->sendMessage("§5>§c Please run this command in-game.");
+            return true;
+        }
+        $this->getListener()->onServerMenu($player);
+    }
+    
+    public function getListener(){
+        return EventListener;
+    }
+}
