d2afeb7276f958fb8820a528893395608d37f530
Enrick3344 <enrico3815@hotmail.fr>
2017-11-11 13:44:38 -0500

    Update EventListener.php

--- src/PrimeItems/EventListener.php
+++ src/PrimeItems/EventListener.php
@@ -8,10 +8,6 @@ use pocketmine\plugin\Plugin;
 use pocketmine\event\Listener;
 use pocketmine\player\Inventory;
 use pocketmine\inventory\BaseInventory;
-use pocketmine\event\player\PlayerRespawnEvent;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerExhaustEvent;
 use jojoe77777\FormAPI;
 use PrimeItems\Main;
 
@@ -25,40 +21,6 @@ class EventListener implements Listener {
      */
     public function __construct(Main $plugin) {
         $this->plugin = $plugin;
-    }
-
-    public function handleJoin(PlayerJoinEvent $ev) {
-        $player = $ev->getPlayer();
-        $inventory = $player->getInventory();
-        $player->getInventory()->clearAll();
-        $inventory->setItem(0, Item::get(Item::COMPASS)->setCustomName("§aServers" . "\n" . "§7Hit to open menu"));
-        $inventory->setItem(8, Item::get(Item::DYE, 10)->setCustomName("§dPlayer Visibility: §aOn"));
-    }
-
-    public function handleInteraction(PlayerInteractEvent $ev) {
-        $player = $ev->getPlayer();
-        $inventory = $player->getInventory();
-        $hand = $inventory->getItemInHand();
-        switch ($hand->getId()) {
-            case Item::COMPASS:
-                $this->onServerMenu($player);
-            break;
-            case Item::DYE:
-                if ($hand->getDamage() === 10) {
-                    foreach ($player->getServer()->getOnlinePlayers() as $p) {
-                        $player->hidePlayer($p);
-                    }
-                    $inventory->setItemInHand(Item::get(Item::DYE, 8)->setCustomName("§dPlayer Visibility: §cOff"));
-                    $player->sendMessage("§cHiding all players!");
-                }elseif ($hand->getDamage() === 8) {
-                    foreach ($player->getServer()->getOnlinePlayers() as $p) {
-                        $player->showPlayer($p);
-                    }
-                    $inventory->setItemInHand(Item::get(Item::DYE, 10)->setCustomName("§dPlayer Visibility: §aOn"));
-                    $player->sendMessage("§aRevealing all players!");
-                }
-            break;
-        }
     }
 
     public function onServerMenu($player) {
@@ -94,16 +56,12 @@ class EventListener implements Listener {
         });
         $form->setTitle("Server List");
         $form->setContent("Choose a Server from the list below to transfer there!");
-        $form->addButton("");
+        $form->addButton("Cancel");
         $form->addButton("SkyBlock", 1, "https://i.imgur.com/G4YNcfX.jpg");
         $form->addButton("Prison", 2, "https://i.imgur.com/G4YNcfX.jpg");
         $form->addButton("Factions", 3, "https://i.imgur.com/G4YNcfX.jpg");
         $form->addButton("Survival", 4, "https://i.imgur.com/G4YNcfX.jpg");
         $form->addButton("Creative", 5, "https://i.imgur.com/G4YNcfX.jpg");
         $form->sendToPlayer($player);
-    }
-
-    public function Hunger(PlayerExhaustEvent $event) {
-        $event->setCancelled(true);
     }
 }
